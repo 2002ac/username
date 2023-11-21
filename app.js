4db7b0e9d1d559b4ef44f431ea55d8c3520fbb58
GOD_OF_WOR <ayxanceferov2002ac@gmail.com>
2023-11-21 13:42:42 +0400

    username

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+let ad=document.querySelector("#ad")
+let soyad=document.querySelector("#soy-ad")
+let ataadi=document.querySelector("#ata-adi")
+let tedris=document.querySelector("#tedris")
+let kisi=document.querySelector("#kisi")
+let qadin=document.querySelector("#qadin")
+let gonder=document.querySelector("#gonder")
+
+
